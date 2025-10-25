cb1ed09f3b682ee135cbccc959feccdc57b1b1f9
Jennifer Frick <jenniferfrick@MacBook-Air-von-Jennifer-3.local>
2025-10-25 17:09:18 +0200

    Anmeldung und Registrierung

--- ClientThread.java
+++ ClientThread.java
@@ -22,18 +22,42 @@ public class ClientThread extends Thread {
 
     public void run(){
         this.running = true;
-        while (!this.logIn()) {
-            this.logIn();
+        while (!this.startseite()) {
+            this.startseite();
         }
         this.server.addClientThread(this);
         this.writerThread.write(this.server.getIdOfAvailableClients(this));
         this.readerThread.start();
     }
 
-    public boolean logIn() {
+    public boolean registrieren() {
         try {
-            writerThread.write("Bitte gib deine Anmeldedaten ein: ");
-            String id = readerThread.nextInput();
+            writerThread.write("Bitte gib deinen Benutzernamen ein: "); 
+            String id = readerThread.nextInput(); //liest Eingabe ein, Funktion aus ReadThread
+
+            Boolean result = database.checkName(id);
+
+            if (!result) {
+                this.id = id;
+                writerThread.write("Passwort: ");
+                String pw = readerThread.nextInput();
+                database.registrieren(id, pw); 
+                writerThread.write("Registrierung erfolgreich.");
+                return true;
+            } else {
+                writerThread.write("Dieser Benutzername existiert bereits. Gib bitte einen neuen Benutzernamen ein.");
+                return false;
+            }
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public boolean anmelden(){
+        try {
+            writerThread.write("Bitte gib deinen Benutzernamen ein: "); 
+            String id = readerThread.nextInput(); //liest Eingabe ein, Funktion aus ReadThread
+
             writerThread.write("Passwort: ");
             String pw = readerThread.nextInput();
 
@@ -41,13 +65,40 @@ public class ClientThread extends Thread {
 
             if (result) {
                 this.id = id;
-                writerThread.write("Anmeldung erfolgreich");
-                return result;
+                writerThread.write("Anmeldung erfolgreich.");
+                return true;
             } else {
-                writerThread.write("Anmeldename oder Passwort falsch.");
+                writerThread.write("Der Benutzername oder das Passwort sind falsch.");
                 return false;
             }
         } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public boolean startseite(){
+        try{
+            writerThread.write("Möchtest du dich anmelden oder registrieren?: ");
+            String antwort = readerThread.nextInput(); //Antwort besteht nur aus einem Wort 
+
+            if(antwort.equals("registrieren")){
+                while (!this.registrieren()) {
+                    this.registrieren();
+                }
+                return true;
+            }
+            else if(antwort.equals("anmelden")){
+                while (!this.anmelden()) {
+                    this.anmelden();
+                }
+                return true;
+            }
+            else{
+                writerThread.write("Das ist keine zulässige Antwort");
+                return false;
+            }
+        }
+        catch (IOException e) {
             return false;
         }
     }

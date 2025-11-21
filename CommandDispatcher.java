347c222aabef04740eca58989c4e0aae629b6100
maxkrischeu <max.krischeu@icloud.com>
2025-11-21 08:42:58 +0100

    Kommentare eingefügt

--- CommandDispatcher.java
+++ CommandDispatcher.java
@@ -15,7 +15,7 @@ public class CommandDispatcher extends Thread {
     public void run() {
         System.out.println("Herzlich Willkommen in unserem kleinen Chatsystem. Sie befinden sich im Adminbereich. Für mögliche Befehle bitte 'hilfe' eingeben.");
 
-        while(running) {
+        while(this.running) {
             String cmd = scanner.nextLine();
 
             switch(cmd) {
@@ -55,6 +55,6 @@ public class CommandDispatcher extends Thread {
                     break;
             }
         }
-        System.out.println("Das Programm und der Server wurden beendet.");
+        System.out.println("Das Programm wurde beendet.");
     }
 }
